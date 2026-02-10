d78c00e22320ff83c14ca434ed22ab569a338edc
aksoumtarik7-beep <>
2026-02-10 15:16:17 +0000

    Challenge: Transformation Grid

--- section/challenge_transformation_grid/main.py
+++ section/challenge_transformation_grid/main.py
@@ -6,18 +6,18 @@ from sklearn.preprocessing import StandardScaler
 data = sns.load_dataset("titanic")
 
 # 2. Fill missing values
-data["age"].fillna(___, inplace=True)
-data["embarked"].fillna(___, inplace=True)
+data["age"].fillna(data["age"].mean(), inplace=True)
+data["embarked"].fillna(data["embarked"].mode()[0], inplace=True)
 
 # 3. Encode 'sex' and 'embarked' using pd.get_dummies()
-data = pd.get_dummies(data, columns=["___", "___"], drop_first=True)
+data = pd.get_dummies(data, columns=["sex", "embarked"], drop_first=True)
 
 # 4. Scale 'age' and 'fare' using StandardScaler
 scaler = StandardScaler()
-data[["age_scaled", "fare_scaled"]] = scaler.fit_transform(data[["___", "___"]])
+data[["age_scaled", "fare_scaled"]] = scaler.fit_transform(data[["age", "fare"]])
 
 # 5. Create new column 'family_size'
-data["family_size"] = ___
+data["family_size"] = data["sibsp"] + data["parch"] + 1
 
 # Save transformed dataset
 transformed_data = data.copy()
